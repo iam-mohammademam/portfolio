15c4a49149d3d14ce6a5be0656d6b1577ba13ab0
iam-mohammademam <abir141578@gmail.com>
2024-12-16 10:04:50 +0600

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
+  output: "export",
+  basePath: "/portfolio", // Replace with your repo name
+  assetPrefix: "/portfolio",
 };
 
 export default nextConfig;
